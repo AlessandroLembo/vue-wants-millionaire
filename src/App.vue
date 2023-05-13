108906488d48b34b06683019824d93c81c5f3ecd
AlessandroLembo <alessandrolembo2@gmail.com>
2023-05-13 19:13:33 +0200

    import container component and move script

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
+<script>
+import ContainerGame from "./components/ContainerGame.vue";
+export default {
+  name: "AppVue",
+  components: {
+    ContainerGame,
+  },
+};
+</script>
+
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <div class="container">
+      <container-game></container-game>
+
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+@use "./assets/scss/style.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
+
