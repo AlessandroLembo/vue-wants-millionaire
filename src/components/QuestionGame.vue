6d106561f538cc1989a108630f1ecbb8d27b5de8
AlessandroLembo <alessandrolembo2@gmail.com>
2023-05-15 17:29:42 +0200

    resposnive layout

--- src/components/QuestionGame.vue
+++ src/components/QuestionGame.vue
@@ -25,16 +25,19 @@ export default {
     <div class="content-quiz">
         <div class="row justify-content-center">
             <div class="col-12">
-                <div id="jumbo-millionaire"></div>
+                <img src="../assets/img/millionaire.jpg" class="img-fluid" alt="millionaire">
                 <div class="question-content">
-                    <h2 class="text-center my-5">{{ getItemRandom.question }}</h2>
-                    <ul class="row">
+                    <h2 class="text-center text-white my-5">{{ getItemRandom.question }}</h2>
+                    <ul class="row justify-content-center align-items-center p-0">
                         <li v-for="answer in getAnswers" :key="answer.answer"
-                            class="col-12 col-sm-6 list-group-item text-center p-2">
-                            <div class="d-flex justify-content-start align-items-center box-answer px-5">
-                                <input type="radio" class="me-4" id="city" name="city">
-                                <div class="d-flex align-items-center">
-                                    <p class="m-0">{{ answer.answer }}</p>
+                            class="col-12 col-sm-5 list-group-item text-center p-2">
+                            <div
+                                class="row justify-content-center justify-content-md-start align-items-center box-answer g-2 p-0 answer">
+                                <input type="radio" class="col-12 col-md-6 mx-md-4 mx-1" id="city" name="city">
+                                <div class="col-12 col-md-6">
+                                    <div class="d-flex align-items-center justify-content-center justify-content-md-start">
+                                        <p class="m-0 fs-6 text-white">{{ answer.answer }}</p>
+                                    </div>
                                 </div>
                             </div>
                         </li>
@@ -47,26 +50,20 @@ export default {
 
 <style scoped lang="scss">
 .content-quiz {
-    width: 1200px;
+    max-width: 1200px;
     min-height: 700px;
     border: 2px solid darkgrey;
 }
 
-#jumbo-millionaire {
-    height: 600px;
-    background-image: url('../assets/img/millionaire.jpg');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
-
 .question-content {
     padding: 2rem;
+    background-color: darkblue;
 }
 
 .box-answer {
     border: 2px solid goldenrod;
     height: 70px;
+    border-radius: 20px;
 
 }
 
@@ -74,5 +71,9 @@ export default {
     width: 25px;
     height: 25px;
     border-radius: 50%;
+}
+
+.answer:hover {
+    background-color: dodgerblue;
 }
 </style>
