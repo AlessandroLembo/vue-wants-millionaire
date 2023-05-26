5fcc1215abf32df51d9fa176ab9694d42e08e6ae
AlessandroLembo <alessandrolembo2@gmail.com>
2023-05-26 17:27:26 +0200

    create component for single answer and move data

--- src/components/AnswersGame.vue
+++ src/components/AnswersGame.vue
@@ -1,7 +1,8 @@
 <script>
-
+import SingleAnswer from './SingleAnswer.vue';
 export default {
     name: "AnswersGame",
+    components: { SingleAnswer },
     props: {
         answers: Array,
         isExactly: Boolean,
@@ -10,11 +11,13 @@ export default {
         disabledRadio: Boolean,
         disabledButton: Boolean
     },
-    data() {
-        return {
-            userAnswer: ''
+
+    methods: {
+        passUserChoose(userAnswer) {
+            this.$emit('user-choose', userAnswer)
         }
     },
+
 
     emits: ['user-choose', 'continue-game'],
 
@@ -30,31 +33,25 @@ export default {
 
                 <!-- controllo la risposta e assegno al blocco un colore diverso a seconda che sia giusta o meno -->
                 <div :class="{ 'bg-success': answer.isclicked && isExactly || isClicked && answer.rightAnswer, 'bg-danger': answer.isclicked && !answer.rightAnswer && isWrong }"
-                    class="row justify-content-center justify-content-md-start align-items-center box-answer g-2 p-0 answer">
-                    <input type="radio" class="col-12 col-md-6 mx-md-4 mx-1 choose-user" :id="answer.answer"
-                        :value="answer.answer" v-model="userAnswer" name="city" @change="$emit('user-choose', userAnswer)"
-                        :disabled="disabledRadio">
-                    <label :for="answer.answer" class="col-12 col-md-6">
-                        <div class="d-flex align-items-center justify-content-center justify-content-md-start">
-                            <p class="m-0 fs-6 city-option">{{ answer.answer.toUpperCase() }}</p>
-                        </div>
-                    </label>
+                    class="box-answer g-2 p-0 answer">
+
+                    <single-answer :answer="answer" :disabledRadio="disabledRadio"
+                        @user-choose="passUserChoose"></single-answer>
                 </div>
             </li>
         </ul>
 
-        <!-- alert che da un feedback in caso di risposta sbagliata -->
+        <!-- alert che dà un feedback in caso di risposta sbagliata -->
         <div v-if="!isExactly && isClicked"
             class="alert alert-danger d-flex flex-column flex-sm-row justify-content-center justify-content-sm-between align-items-center fs-3"
             role="alert">
             Risposta errata! Vai alla prossima domanda..
-            <button type="button" class="btn btn-outline-secondary"
-                @click="$emit('continue-game', userAnswer)">Continua</button>
+            <button type="button" class="btn btn-outline-secondary" @click="$emit('continue-game')">Continua</button>
         </div>
         <div v-else-if="!isClicked || isExactly"
             class="d-flex justify-content-center justify-content-sm-end align-items-center">
             <button type="button" class="btn btn-warning mt-2" :disabled="!disabledButton"
-                @click="$emit('continue-game', userAnswer)">Continua</button>
+                @click="$emit('continue-game')">Continua</button>
         </div>
     </div>
 </template>
@@ -67,17 +64,7 @@ export default {
 
 }
 
-.choose-user {
-    width: 25px;
-    height: 25px;
-    border-radius: 50%;
-}
-
 .answer:hover {
     background-color: dodgerblue;
-}
-
-.city-option {
-    color: white;
 }
 </style>
---
+++ src/components/SingleAnswer.vue
@@ -0,0 +1,51 @@
+<script>
+
+export default {
+    name: "SingleAnswer",
+    props: {
+        answer: Object,
+        disabledRadio: Boolean,
+    },
+    data() {
+        return {
+            userAnswer: ''
+        }
+    },
+
+    computed: {
+        getA() {
+            console.log(this.userAnswer);
+            return this.answer
+        }
+    },
+
+    emits: ['user-choose'],
+
+
+}
+</script>
+
+<template>
+    <!-- tramite prop ricevo l'oggetto answer generato dal giro sull'array e stampo la risposta in pagina -->
+    <div class="row justify-content-center justify-content-md-start align-items-center h-100">
+        <input type="radio" class="col-12 col-md-6 mx-md-4 mx-1 choose-user" :id="answer.answer" :value="answer.answer"
+            v-model="userAnswer" name="city" @change="$emit('user-choose', userAnswer)" :disabled="disabledRadio">
+        <label :for="answer.answer" class="col-12 col-md-6">
+            <div class="d-flex align-items-center justify-content-center justify-content-md-start">
+                <p class="m-0 fs-6 city-option">{{ answer.answer.toUpperCase() }}</p>
+            </div>
+        </label>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.choose-user {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+}
+
+.city-option {
+    color: white;
+}
+</style>
