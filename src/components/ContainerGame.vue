2b52c2865a71fb08617cac4cf58ec739af575a1a
AlessandroLembo <alessandrolembo2@gmail.com>
2023-05-26 13:36:24 +0200

    some changes in temaple

--- src/components/ContainerGame.vue
+++ src/components/ContainerGame.vue
@@ -144,84 +144,88 @@ export default {
 
 <template>
     <div class="content-quiz h-100">
-        <div class="box-image">
-            <img src="../assets/img/millionaire.jpg" class="img-fluid" alt="millionaire">
-        </div>
-        <div class="question-content">
-            <div class="container">
-                <!-- Se il gioco è finito mostro i risultati -->
-                <div v-if="gameOver" class="d-flex justify-content-center">
+        <div v-if="!result">
+            <div class="box-image">
+                <img src="../assets/img/millionaire.jpg" class="img-fluid" alt="millionaire">
+            </div>
+            <div class="question-content">
+                <div class="container">
+                    <!-- Se il gioco è finito mostro i risultati -->
+                    <div v-if="gameOver" class="d-flex justify-content-center">
 
-                    <!-- button pe mostrare il punteggio -->
-                    <!-- TODO: FARLO DIVENTARE UN ROUTER LINK CHE PORTA ALLA PAGINA DEI RISULTATI -->
-                    <button v-if="!result" type="button" class="btn btn-secondary mt-2" @click="showResult()">Vai al
-                        punteggio
-                    </button>
-
-                    <!-- resoconto della partita -->
-                    <div v-else>
-                        <div class="result">
-                            <div class="me-3">
-                                <!-- caso in cui l'utemte dà tutte le risposte esatte -->
-                                <h3 v-if="!userLose.length">Complimenti!!! Hai risposto esattamente a
-                                    tutte e {{ userWin.length }} le domande.</h3>
-
-                                <!-- almeno una risposta esatta... ma anche almeno una sbagliata... -->
-                                <div v-else-if="userWin.length && userLose.length">
-                                    <h3>Hai risposto esattamente a {{ userWin.length }}
-                                        <span v-if="userWin.length === 1">sola domanda</span>
-                                        <span v-else>domande</span>
-                                    </h3>
-
-                                    <ul v-for="choose in userWin" :key="choose.userChoose">
-                                        <li class="list-group-item">Alla domanda {{ `"${choose.question}"`
-                                        }} hai
-                                            risposto con <span class="text-success">{{ choose.userChoose
-                                            }}</span>
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <!-- caso in cui l'utente dà tutte le risposte sbagliate -->
-                                <h3 v-if="!userWin.length">Brutto risultato!!! Purtroppo ha sbagliato tutte e {{
-                                    userLose.length }} le risposte.</h3>
-
-                                <!-- almeno una risposta esatta... ma anche almeno una sbagliata... -->
-                                <div v-else-if="userWin.length && userLose.length">
-                                    <h3 v-if="userLose.length === 1">Buona prova, hai sbagliato solo {{
-                                        userLose.length
-                                    }}
-                                        risposta</h3>
-                                    <h3 v-else>Purtroppo hai sbagliato {{ userLose.length }} risposte</h3>
-
-                                </div>
-
-                                <!-- mostro le risposte sbagliate e l'opzione che sarebbe stata giusta -->
-                                <ul v-for="choose in userLose" :key="choose.userAnswer">
-                                    <li class="list-group-item">Alla domanda {{ `"${choose.question}"` }} hai
-                                        risposto
-                                        <span class="text-danger">{{ choose.userChoose }}. </span>
-                                        <span>La risposta corretta era <span class="text-success">{{
-                                            choose.rightAnswer }}</span></span>
-                                    </li>
-                                </ul>
-
-                            </div>
-                            <div class="d-flex justify-content-end">
-                                <a href="http://localhost:8080/"><button type="button" class="btn btn-warning">Nuova
-                                        partita</button></a>
-                            </div>
+                        <!-- button pe mostrare il punteggio -->
+                        <div>
+                            <button type="button" class="btn btn-secondary mt-2" @click="showResult()">Vai al
+                                punteggio
+                            </button>
                         </div>
                     </div>
+
+                    <!-- ...altrimenti vado avanti col gioco -->
+                    <div v-else>
+                        <question-game :question="getItemRandom"></question-game>
+
+                        <answers-game :answers="getAnswers" :isExactly="isExactly" :isClicked="isClicked" :isWrong="isWrong"
+                            :disabledRadio="disabledRadio" :disabledButton="disabledButton" @user-choose="getUserAnswer"
+                            @continue-game="playAgain"></answers-game>
+                    </div>
                 </div>
+            </div>
 
-                <!-- ...altrimenti vado avanti col gioco -->
-                <div v-else>
-                    <question-game :question="getItemRandom"></question-game>
+        </div>
+        <!-- resoconto della partita -->
+        <div v-else>
+            <h1 class="text-center result-text mt-5">PUNTEGGIO TOTALE</h1>
+            <div class="result container mt-5">
+                <div class="me-3">
+                    <!-- caso in cui l'utemte dà tutte le risposte esatte -->
+                    <h3 v-if="!userLose.length">Complimenti!!! Hai risposto esattamente a
+                        tutte e {{ userWin.length }} le domande.</h3>
 
-                    <answers-game :answers="getAnswers" :isExactly="isExactly" :isClicked="isClicked" :isWrong="isWrong"
-                        :disabledRadio="disabledRadio" :disabledButton="disabledButton" @user-choose="getUserAnswer"
-                        @continue-game="playAgain"></answers-game>
+                    <!-- almeno una risposta esatta... ma anche almeno una sbagliata... -->
+                    <div v-else-if="userWin.length && userLose.length">
+                        <h3>Hai risposto esattamente a {{ userWin.length }}
+                            <span v-if="userWin.length === 1">sola domanda</span>
+                            <span v-else>domande</span>
+                        </h3>
+
+                        <ul v-for="choose in userWin" :key="choose.userChoose">
+                            <li class="list-group-item">Alla domanda {{ `"${choose.question}"`
+                            }} hai
+                                risposto con <span class="text-success">{{ choose.userChoose
+                                }}</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- caso in cui l'utente dà tutte le risposte sbagliate -->
+                    <h3 v-if="!userWin.length">Brutto risultato!!! Purtroppo ha sbagliato tutte e {{
+                        userLose.length }} le risposte.</h3>
+
+                    <!-- almeno una risposta esatta... ma anche almeno una sbagliata... -->
+                    <div v-else-if="userWin.length && userLose.length">
+                        <h3 v-if="userLose.length === 1">Buona prova, hai sbagliato solo {{
+                            userLose.length
+                        }}
+                            risposta</h3>
+                        <h3 v-else>Purtroppo hai sbagliato {{ userLose.length }} risposte</h3>
+
+                    </div>
+
+                    <!-- mostro le risposte sbagliate e l'opzione che sarebbe stata giusta -->
+                    <ul v-for="choose in userLose" :key="choose.userAnswer">
+                        <li class="list-group-item">Alla domanda {{ `"${choose.question}"` }} hai
+                            risposto
+                            <span class="text-danger">{{ choose.userChoose }}. </span>
+                            <span>La risposta corretta era <span class="text-success">{{
+                                choose.rightAnswer }}</span></span>
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="d-flex justify-content-end">
+                    <a href="http://localhost:8080/"><button type="button" class="btn btn-warning">Nuova
+                            partita</button></a>
                 </div>
             </div>
         </div>
@@ -236,6 +240,12 @@ export default {
 .question-content {
     padding: 2rem;
     background-color: darkblue;
+}
+
+.result-text {
+    color: darkgoldenrod;
+    font-size: 60px;
+    font-weight: bold;
 }
 
 .result {
